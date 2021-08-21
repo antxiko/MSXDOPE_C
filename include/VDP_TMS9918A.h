3d1f0e935878b8afd7200d961b41901305855540
Antxiko Gorjon <antxiko@gmail.com>
2021-08-21 20:17:38 +0200

    SCREEN 2 RANDOM PRECIOS

--- include/VDP_TMS9918A.h
+++ include/VDP_TMS9918A.h
@@ -1,21 +1,8 @@
 /* =============================================================================
-  SDCC MSX ROM VDP TMS9918A Functions Library (object type)
-  Version: 1.0
-  Author: mvac7/303bcn
-  Architecture: MSX
-  Format: C Object (SDCC .rel)
-  Programming language: C
-  WEB: -
-  mail: mvac7303b@gmail.com
-
-  Description:
-    Open Source library of functions to work with the TMS9918A video processor 
-    
-  History of versions:
-    v1.0 (14/02/2014) current version
+  VDP TMS9918A MSX ROM SDCC Library (fR3eL Project)
 ============================================================================= */
-#ifndef  __VDP_TMS9918_H__
-#define  __VDP_TMS9918_H__
+#ifndef  __VDP_TMS9918A_H__
+#define  __VDP_TMS9918A_H__
 
 
 
@@ -95,6 +82,14 @@
 
 
 
+// boolean
+#ifndef _BOOLEAN
+#define _BOOLEAN
+  typedef enum {false = 0, true = 1} boolean;
+#endif
+
+
+
 
 /* =============================================================================
  SCREEN
@@ -102,7 +97,27 @@
  Input      : [char] number of screen mode
  Output     : -
 ============================================================================= */
-extern void SCREEN(char);
+void SCREEN(char mode);
+
+
+
+/* =============================================================================
+ SetSpritesSize
+ Description: Set size type for the sprites.
+ Input:       [char] size: 0=8x8; 1=16x16
+ Output:      -
+============================================================================= */ 
+void SetSpritesSize(char size);
+
+
+
+/* =============================================================================
+ SetSpritesZoom
+ Description: Set zoom type for the sprites.
+ Input:       [char] zoom: false/0 = x1; true/1 = x2
+ Output:      -
+============================================================================= */
+void SetSpritesZoom(boolean zoom);
 
 
 
@@ -114,7 +129,7 @@ extern void SCREEN(char);
               [char] border color
  Output     : -     
 ============================================================================= */
-extern void COLOR(char, char, char);
+void COLOR(char ink, char background, char border);
 
 
 
@@ -125,7 +140,7 @@ extern void COLOR(char, char, char);
               [char] value
  Output     : - 
 ============================================================================= */
-extern void VPOKE(unsigned int, char);
+void VPOKE(unsigned int vaddr, char value);
 
 
 
@@ -135,7 +150,7 @@ extern void VPOKE(unsigned int, char);
  Input      : [unsigned int] VRAM address
  Output     : [char] value
 ============================================================================= */ 
-extern char VPEEK(unsigned int);
+char VPEEK(unsigned int vaddr);
 
 
 
@@ -147,7 +162,7 @@ extern char VPEEK(unsigned int);
               [char] Value to fill.
  Output     : - 
 ============================================================================= */
-extern void FillVRAM(unsigned int, unsigned int, char);
+void FillVRAM(unsigned int vaddr, unsigned int length, char value);
 
 
 
@@ -159,7 +174,7 @@ extern void FillVRAM(unsigned int, unsigned int, char);
               [unsigned int] blocklength
  Output     : - 
 ============================================================================= */
-extern void CopyToVRAM(unsigned int, unsigned int, unsigned int);
+void CopyToVRAM(unsigned int addr, unsigned int vaddr, unsigned int length);
 
 
 
@@ -171,7 +186,7 @@ extern void CopyToVRAM(unsigned int, unsigned int, unsigned int);
               [unsigned int] blocklength
  Output     : -             
 ============================================================================= */
-extern void CopyFromVRAM(unsigned int, unsigned int, unsigned int);
+void CopyFromVRAM(unsigned int vaddr, unsigned int addr, unsigned int length);
 
 
 
@@ -182,7 +197,7 @@ extern void CopyFromVRAM(unsigned int, unsigned int, unsigned int);
               [char] value
  Output     : -             
 ============================================================================= */
-extern void SetVDP(char, char);
+void SetVDP(char reg, char value);
 
 
 
---
+++ src/VDP_PRINT.c
@@ -0,0 +1,237 @@
+/* =============================================================================
+   SDCC VDP PRINT Lib v1.2 (16/April/2021)
+   Author: mvac7
+   Architecture: MSX
+   Format: .rel (SDCC object file)
+   Programming language: C
+   
+   Description:
+     Functions Library for display text strings in the graphic modes of the 
+     TMS9918A (G1 and G2).
+     
+   History of versions:
+   - v1.2 (16/April/2021) VPRINTN string size control
+   - v1.1 (9/Sep/2020) 
+   - v1   (4/Feb/2016) First version     
+============================================================================= */
+#include "../include/VDP_PRINT.h"
+#include "../include/VDP_TMS9918A.h"
+
+
+char CSTATE;
+
+
+
+/* =============================================================================
+ It provides the address of the video memory map tiles, from the screen position
+ indicated.
+ Inputs:
+   column (byte) 0 - 31
+   line (byte) 0 - 23
+============================================================================= */
+unsigned int GetVRAMaddressByPosition(char column, char line)
+{
+   return BASE10 + (line*32)+column;
+}
+
+
+
+/* =============================================================================
+ Prints a string at the indicated screen position
+ Inputs:
+   column (byte) 0 - 31
+   line (byte) 0 - 23
+   text (char*) string
+============================================================================= */
+void VPRINT(char column, char line, char* text)
+{
+  unsigned int vaddr = GetVRAMaddressByPosition(column, line);
+  VPrintString(vaddr, text);
+}
+
+
+
+/* =============================================================================
+ Prints a string at the indicated screen position
+ Inputs:
+   column (byte) 0 - 31
+   line (byte) 0 - 23
+   text (char*) string
+   length (uint) length of the string to print
+============================================================================= */
+void VPRINTN(char column, char line, char* text, unsigned int length)
+{
+  unsigned int vaddr = GetVRAMaddressByPosition(column, line);
+  //CopyToVRAM((unsigned int) text, vaddr, length);
+  while(*(text) && length-->0)  VPOKE(vaddr++,*(text++));
+
+}
+
+
+
+/* =============================================================================
+ Dump the contents of an array of char in a position shown in the video memory
+ Inputs:
+   vaddr (uint) 
+   text (char*) array
+============================================================================= */
+void VPrintString(unsigned int vaddr, char* text)
+{
+  char length=0;
+  while(text[length]) length++;
+  //while(*(text)) VPOKE(vaddr++,*(text++));
+  CopyToVRAM((unsigned int) text, vaddr, length);
+}
+
+
+
+/* =============================================================================
+   VPrintNumber
+   Prints a number at the specified position on the screen.
+   
+   Inputs:
+     [char] column 0 - 31
+     [char] line   0 - 23
+     [unsigned int] number
+     [char] length
+============================================================================= */
+void VPrintNumber(char column, char line, unsigned int value, char length)
+{
+  unsigned int vaddr = GetVRAMaddressByPosition(column, line);
+
+  VPrintNum(vaddr, value, length);
+}
+
+
+/* =============================================================================
+   VPrintNum
+   Prints a number at the specified position on the screen.
+   
+   Inputs:
+     [unsigned int] VRAM address in Pattern Name Table.
+     [unsigned int] number
+     [char] length
+============================================================================= */
+void VPrintNum(unsigned int vaddr, unsigned int value, char length)
+{
+  char pos=5-length;
+  char text[]="     ";
+
+  num2Dec16(value, text); 
+  
+  //while (length-->0){ VPOKE(vaddr++,text[pos++]);}
+  CopyToVRAM((unsigned int) text + (5-length), vaddr, length);
+}
+
+
+
+/* =============================================================================
+ 16-bit Integer to ASCII (decimal)
+ original code by baze http://baze.sk/3sc/misc/z80bits.html#5.1
+ (update) Add functionality to replace leading zeros by spaces.  
+ Input: HL = number to convert, DE = location of ASCII string
+ Output: ASCII string at (DE)
+============================================================================= */
+void num2Dec16(unsigned int aNumber, char *address) __naked
+{
+  aNumber;
+  address;
+__asm
+  push IX
+  ld   IX,#0
+  add  IX,SP
+  
+  ld   L,4(ix)
+  ld   H,5(ix)
+  
+  ld   E,6(ix)
+  ld   D,7(ix)
+  
+  ld   A,#32         ;ASCII value for space
+  ld   (#_CSTATE),A
+  	
+  ld   BC,#-10000
+  call $Num1
+  ld   BC,#-1000
+  call $Num1
+  ld   BC,#-100
+  call $Num1
+  ld   C,#-10
+  call $Num1
+  ld   C,B
+  call $Num1
+
+  ;END
+  pop  IX
+  ret
+    
+$Num1:	
+  ld   A,#47 ; 0 ASCII char - 1
+   
+$Num2:	
+  inc  A
+  add  HL,BC
+  jr   C,$Num2
+  
+  sbc  HL,BC
+  
+  cp   #48       
+  jr   NZ,$Num3  ;if A!=0 then goto $Num3
+  
+  ld   A,(#_CSTATE)
+  jr   $Num4
+
+
+$Num3:
+  push AF
+  ld   A,#48  ;ASCII value for 0
+  ld   (#_CSTATE),A
+  pop  AF	
+	
+$Num4:	
+  ld   (DE),A
+  inc  DE
+  	
+  ret
+  
+__endasm;
+}
+
+
+/*void VPRINTO(byte column, byte line, char* text, char offset)
+{
+  uint vaddr = GetVRAMaddressByPosition(column, line);
+  vpokeBlockOffset(vaddr, text,offset);
+}*/
+
+
+/*void vpokeBlockOffset(uint vaddr, char* text, char offset)
+{
+  while(*(text)) vpoke(vaddr++,*(text++)+offset);
+}*/
+
+
+
+/* =============================================================================
+ muestra un texto en la pantalla de varias lineas separadas con \n
+ ejem: vPrintLines(10,10,"ARE YOU SURE\nYOU WANT TO\nDELETE SONG?");
+============================================================================= */
+/*void vprintLines(byte column, byte line, char* text)
+{
+  char character;
+  uint vaddr = GetVRAMaddressByPosition(column, line);
+  while(*(text))
+  {
+    character=*(text++);
+    if (character=='\n') //return
+    {
+      //next line
+      line++;
+      vaddr = GetVRAMaddressByPosition(0, line);
+    }
+    else{
+      vpoke(vaddr++,character);
+    }    
+  }
+  return;
+}*/
